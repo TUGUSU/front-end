354dc95dda1e55c6dde18a7d1c38f07f61ad33da
Tugstuguldur.G <163750263+TUGUSU@users.noreply.github.com>
2026-04-21 00:57:06 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    SOAP_BASE_URL: "http://18.138.115.64:3000/api/soap",
-    JSON_BASE_URL: "http://18.138.115.64:3000/api/users",
-    FILE_BASE_URL: "http://18.138.115.64:3000/api/files"
+    SOAP_BASE_URL: "http://168.144.33.200:3000/api/soap",
+    JSON_BASE_URL: "http://168.144.33.200:3000/api/users",
+    FILE_BASE_URL: "http://168.144.33.200:3000/api/files"
 };
