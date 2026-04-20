d25e5d8619175599ba3013bff49d0f365b8cfd2e
Ganbaatar Tugstuguldur <tugstuguldur94548950@gmail.com>
2026-04-20 10:45:51 +0800

    Route frontend requests through API gateway

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    SOAP_BASE_URL: "http://18.138.115.64:8080/ws",
-    JSON_BASE_URL: "http://18.138.115.64:8081/users",
-    FILE_BASE_URL: "http://18.138.115.64:8082/files"
+    SOAP_BASE_URL: "http://18.138.115.64:3000/api/soap",
+    JSON_BASE_URL: "http://18.138.115.64:3000/api/users",
+    FILE_BASE_URL: "http://18.138.115.64:3000/api/files"
 };
